7219bc74f1075f8e099fdc276091348d12b30d44
GeunYoung Jang <zzbtang@gmail.com>
2022-12-17 23:54:00 +0900

    221217_nest_Create_Read

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    // @Body('title') title: string,
+    // @Body('description') description: string,
+    @Body() createBoardDto: CreateBoardDto,
+  ): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Query('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,32 @@
+import { BoardStatus } from './board.model';
 // 서비스 안에서는 데이터베이스 관련된 로직을 처리
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+}
